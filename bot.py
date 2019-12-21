cc893eb910d3cbf35823660ffab2bcd6274116ae
CubicFace <thedjplays.gaming@gmail.com>
2019-12-22 00:06:35 +0100

    .

--- bot.py
+++ bot.py
@@ -10,9 +10,10 @@ import os
 client = commands.Bot(command_prefix=".s ", owner_id=332082083604463616)
 
 client.remove_command('help')
+guild_id=656176795128692736
+welcome=656176795128692739
+bot_chnl_id=658069670469042206
 
-welcome=discord.Object(id='656176795128692739')
-bot_chnl=discord.Object(id='658069670469042206')
 @client.event
 async def on_ready():
     print("[INFO] On.")
@@ -21,7 +22,8 @@ async def on_ready():
     User ID: {client.user.id}
     Name: {client.user.name}
     """)
-    await client.send_message(bot_chnl,"The Snom is connected! <:NATSUKISPARKLE:656602806974808074>")
+    channel=client.get_channel(bot_chnl_id)
+    await channel.send("The Snom is connected! <:NATSUKISPARKLE:656602806974808074>")
 
 @client.event
 async def on_member_join(member):
