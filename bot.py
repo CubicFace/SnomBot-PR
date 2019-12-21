e0518b165f994599089c049b31424d7ef7ad1679
CubicFace <thedjplays.gaming@gmail.com>
2019-12-21 14:47:49 +0000

    changed token

--- bot.py
+++ bot.py
@@ -62,4 +62,4 @@ async def stop_bot(ctx):
 
 
 keep_alive()
-client.run("NjU2OTM3NTU3MDk0ODI2MDE0.Xf4u2w.aW6AwMYqzOXvZxm9AevqCriOvCo")
+client.run('NjU2OTM3NTU3MDk0ODI2MDE0.Xf4weA.lAuSJ0d_I25zwQQceYWljztubY4')
