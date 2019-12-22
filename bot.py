dfbd52575011994774c078905a2800b106c7e623
CubicFace <thedjplays.gaming@gmail.com>
2019-12-22 19:18:00 +0100

    1.0.0.3: say command

--- bot.py
+++ bot.py
@@ -163,12 +163,15 @@ async def naughty(ctx):
     await ctx.send("<:ANGWYSNOM:656753233968234516> The Snom is __**Christian**__ :cross: ")
 
 @client.command(brief="Make the Snom say something.", usage="Arguments:\n<*, say>: The thing you want to say")
-async def say(ctx, *,say):
+async def say(ctx, id=None, *,say):
     """
     Make the Snom say something
     Also it's indirect an use of Administrator role.
     """
-    await ctx.say(say)
+    if id != None:
+        channel=client.get_channel(id)
+        await channel.send(say)
+    await ctx.send(say)
 
 @client.command(name="stop", brief="[DEV]This completely stop the bot.", usage="No argument required but being the developer is required :)")
 async def stop_bot(ctx):
